aff6cc4fd4a127477b282609f16ebee94e261105
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-11-19 12:02:52 -0500

    add link to admin dashboard

--- front-end/src/components/Navbars/GlobalNavbar.js
+++ front-end/src/components/Navbars/GlobalNavbar.js
@@ -83,6 +83,15 @@ export default function Navbar(props) {
 
                 </Link>
               </li>
+              <li className="flex items-center">
+                <Link className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  to="admin/dashboard"
+                  target="_self"
+                >
+                  Admin Dashboard
+
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
