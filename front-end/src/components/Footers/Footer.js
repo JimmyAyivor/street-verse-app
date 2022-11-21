ee3e301eebc448f271cbe383570957912ab1be2a
T.K <nique@Niques-MacBook-Air.local>
2022-11-20 19:18:24 -0500

    social media links

--- front-end/src/components/Footers/Footer.js
+++ front-end/src/components/Footers/Footer.js
@@ -48,7 +48,7 @@ export default function Footer() {
                   className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
                 >
-                  <a href="" ><i className="fab fa-dribbble"></i></a>
+                  <a href="" ><i class="fa-brands fa-linkedin-in"></i></a>
                 </button>
                 <button
                   className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
