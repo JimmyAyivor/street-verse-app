f980231b6526c02d582f3fd71e8946eedb2f2b88
Dannell <dannellmorales@pursuit.org>
2022-11-27 13:09:23 -0500

    Terms/Privacy/Back to Top Button

--- front-end/src/pages/privacy.js
+++ front-end/src/pages/privacy.js
@@ -16,19 +16,19 @@ export default function Privacy() {
       <br></br>
       <p>By using StreetVerse’s website and mobile application, you indicate that you understand and agree to this Privacy Policy. If you do not agree with the terms of this Privacy Policy, please do not use this website and/or mobile application.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">1. Scope and Consent</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">1. Scope and Consent</h2>
       <p>You accept this Privacy Policy when you sign up for, access, or use our products, services, content, features, technologies or functionality, features offered through this website and mobile application (hereinafter referred to as “Services”). This Privacy Policy intended to govern the use of the website and mobile application and it appears in the footer of our website and mobile application.</p>
       <br></br>
       <p>By using our Services and/or registering for an account with us, you are accepting this Privacy Policy and Terms of Use, you are consenting to our collection, use, disclosure, retention, transfer, and protection of your information and personal information as described in this Privacy Policy. If you choose not to provide the information we require, you may not be able to take advantage of many of our features of the website and our Services.</p>
       <br></br>
       <p>Your privacy is of utmost importance to us. We do not publish, sell or rent your personal information to third parties for their marketing purposes without your explicit consent.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">2. Personal Information</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">2. Personal Information</h2>
       <p>“Personal Information” is information relating to an identified or identifiable natural person; an identifiable person is one who can be identified, directly or indirectly, in particular by reference to an identifier such as a name, an identification number, location data, online identifier or to one or more factors specific to the physical, physiological, genetic, mental, economic, cultural or social identity of that natural person. We do not consider Personal Information to include information that has been made anonymous or aggregated so that it can no longer be used to identify a specific person, whether in combination with other information or otherwise. As its name suggests, “Sensitive Information” is a more sensitive subset of personal information and if such information is considered sensitive under applicable law then it will be handled in accordance with such laws. Further, we also collect mobile numbers, email id, address, educations records, past employment records.</p>
       <br></br>
       <p>We collect, use, disclose, transfer, and store personal information when needed to provide our Services and for our operational and business purposes as described in this Privacy Policy. We want to be clear about our Privacy Practices so that you can make informed choices about the use of your information, and we encourage you to contact us at any time with questions or concerns.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">3. Information we collect</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">3. Information we collect</h2>
       <p>When you use our website and mobile application, we collect and store your Personal Information that is received from you. Our primary goal in doing so is to provide a safe, secure, efficient, smooth experience while using our Services. This allows us to provide services and features that most likely meet your needs, and to customize our website and mobile application to make your experience safer and easier. Importantly, we only collect Personal Information about you that we consider necessary for achieving a purpose.</p>
       <br></br>
       <p>We also collect information sent to us by your computer, mobile phone, and other access device. The information sent to us includes, but is not limited to, the following: data about the pages you access, computer IP address, device ID or unique identifier, device type, geo-location information, computer and connection information, mobile network information, statistics on page views, traffic to and from the sites, referral URL, ad data, and standard web log data and other information. We also collect anonymous information through our use of cookies and web beacons.</p>
@@ -48,25 +48,25 @@ export default function Privacy() {
       <p>We have necessary procedures and controls to help ensure that personal information is protected no matter what country it is stored in or transferred to.</p>
       <br></br>
 
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">4. Information you give us when you use our Services or register for an account with us.</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">4. Information you give us when you use our Services or register for an account with us.</h2>
       <p>Identifying information such as your name, addresses, identification number, branch office address, telephone numbers, email addresses when you register for an account with us</p>
       <p>If any financial transaction data provided by you shall be retained by us solely in accordance with the provision of applicable data protection laws;
         Additional information be required due to amendment in applicable data protection laws from time to time, such information will be collected. And retained as per applicable data protection laws.</p>
       <p>Billing and related information of your account will be available with us in accordance to the provision of laws and for future reference as per our policy.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">5. Technical and functional management of the Website and mobile application.</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">5. Technical and functional management of the Website and mobile application.</h2>
       <p>When you use our website, we also process technical data such as your IP-address, Device ID or MAC-address, access dates and times, app features or pages viewed, app crashes and information about the manufacturer, model, and operating system of your devices. We use this data to enable us to deliver the functionalities of the website and mobile application, resolve technical difficulties and provide you with the appropriate services (prevent the transmission of spam, viruses and/or unlawful, defamatory, obscene or otherwise undesirable material or content).</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">6. Cookie and its Usage</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">6. Cookie and its Usage</h2>
       <p><li>a) “Cookies” are pieces of information that are stored by your web browser on your computer’s hard disk, for record-keeping purposes. Cookies only identify your computer and not you personally.</li>
         <br></br>
         <li>b) We make use of cookies to store your preferences, record session information, collect information on how you visit and access our Services, and to tailor our Services features and functionality to your needs.  Most web browsers allow you to turn off cookies; however, turning off cookies will limit your use of our Services. If you do accept a “cookie”, you hereby agree to our use of Information collected by us using that cookie.</li></p>
       <br></br>
 
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">7. Personal information from other sources</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">7. Personal information from other sources</h2>
       <p>If you give us personal information about someone else, you must do so only with their explicit and prior consent. You should inform them how we collect, use, disclose, and retain, transfer their personal information according to our Privacy Policy.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">8. Use and Retention of Personal information</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">8. Use and Retention of Personal information</h2>
       <p>We may use the information we collect about you to:</p>
       <br></br>
       <p>Provide you, maintain, and improve our Services, including, for example, to facilitate send receipts, provide products and services you request (and send related information), develop new features, provide customer support to users for website and mobile application (i.e. email, telephone, etc.) develop safety features, authenticate users, and send product updates and administrative messages;</p>
@@ -91,7 +91,7 @@ export default function Privacy() {
       <br></br>
       <p>Subject to applicable law, we shall delete your Personal Information upon your written request; StreetVerse will make reasonable attempts to delete all instances of the information in their entirety.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">9. Disclose of Personal Information</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">9. Disclose of Personal Information</h2>
       <li>a) We may disclose your Personal Information with companies, organizations or individuals outside of StreetVerse in order to deliver our Services to you that, in some cases, are targeted for your interests. We minimize the amount of disclosure of Personal Information, we disclose to what is directly relevant and necessary to accomplish the specified purpose. As stated above, we do not disclose your Personal Information to any third parties for their promotional, marketing and advertising purposes without your explicit consent.</li>
       <br></br>
       <li>b) We may be required to disclose your Personal Information with government authorities and agencies for the purposes of verification of identity or for prevention, detection, investigation including but not limited to cyber incidents, prosecution, and punishment of offenses. You agree and consent for us to disclose your information if so required under the applicable law.</li>
@@ -101,7 +101,7 @@ export default function Privacy() {
 
       <li>d) If we were to merge with or be acquired by another company, sale of company assets or restructuring, financing, or acquisition of all or a portion of our business by or into another company. We may share information with them in accordance with this Privacy Policy. Should such an event occur, we will require that the new combined entity follow this Privacy Policy with respect to your Personal Information. If your Personal Information will be collected, used, disclosed, or retained for any purposes not covered in this Privacy Policy, you will receive prior notification of the processing of your Personal Information for the new purposes.</li>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">10. Choices</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">10. Choices</h2>
       <li>a) You may correct your account information at any time by logging into your online web account. If you wish to cancel or revoke your account, please contact us at our <a href="/contact" class="text-blue-600 hover:text-blue-700 underline transition duration-300 ease-in-out mb-4">
         Contact Page</a> to unsubscribe and we will process your request in the 15 working days. Please note that in some cases we may retain certain information about you as required by law, or for legitimate business purposes to the extent permitted by law.</li>
       <br></br>
@@ -112,40 +112,40 @@ export default function Privacy() {
       <li>d) If you withdraw your consent for the use or disclosure of your Personal Information for purposes set out in this Privacy Policy, you may do so by mailing us on unsubscribe@StreetVersetechin.com and we will address your concern within 15 business days. Though by doing so you may not have access to all our Services and we might not be able to provide you all of the Services as our users and authorized under this Privacy Policy and our <a href="/terms" class="text-blue-600 hover:text-blue-700 underline transition duration-300 ease-in-out mb-4">
         Terms</a> of Use.</li>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">11. Access Rights</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">11. Access Rights</h2>
       <p>To protect your privacy and security, we will verify your identity before granting access or making changes to your personally-identifying information. If you have registered your profile on us, your User ID and Password are required to access your profile information.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">12. Contact Information</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">12. Contact Information</h2>
       <p> We seek permission for our Service for collecting and syncing of contact information from your system and device as per the permission system used through web and mobile operating system. If you initially permit the collection of this information, IOS users can later disable it by changing the contacts settings on respective mobile device. The Android and other platform may or not provide such settings.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">13. Opt-Out & Unsubscribe</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">13. Opt-Out & Unsubscribe</h2>
       <p>You may opt out of receiving update, promotional and marketing messages from us in accordance to applicable law by following the instructions in those messages or contact us on our <a href="/contact" class="text-blue-600 hover:text-blue-700 underline transition duration-300 ease-in-out mb-4">
         Contact Page</a>. If you opt out, we may still send you non-promotional communications, such as those about your account, about Services you have requested, or our ongoing business relations.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... "> 14. Security and Storage Measure</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... "> 14. Security and Storage Measure</h2>
       <li> a) We seek to store your Personal Information in secure operating environments that are not accessible to the general public. We also ensure that the security measures are in place to protect against the loss, unauthorized access, misuse or alteration of your Personal Information by our employees or third parties, however, no data transmission over the Internet or data storage environment can be guaranteed to be 100% secure, so we cannot give an absolute assurance that the information you provide to us will be secure at all times.</li>
       <br></br>
       <li>b) We will not be held responsible for events arising from unauthorized access to your Personal Information. We will rely on you for telling us if you experience any unusual events that may indicate a breach in your information security. We will then seek to investigate whether the security breach was related to the data transmissions from our Services and let you know what steps can be taken to rectify the problem. Further action, such as reporting incidents to the police or other relevant authorities, may also be required.</li>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">15. Children's Privacy</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">15. Children's Privacy</h2>
       <p>Our Services are generally not aimed at children. If, however, we collect and use information about children, such as to develop an educational resource, we will comply with industry guidelines and applicable data protection laws.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">16. Third Party Privacy Practices</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">16. Third Party Privacy Practices</h2>
       <p>This Privacy Policy addresses only the use and disclosure of Personal Information we collect from you. If you disclose your information to others, or if you are directed to a third-party website, their privacy policy and practices will apply. You should review the privacy policies applicable to these third-party services.</p>
       <br></br>
       <p>We cannot guarantee the privacy or security of your information once you provide it to a third party and we encourage you to evaluate the privacy and security policies of third party before entering into a transaction and choosing to share your information.</p>
       <br></br>
       <p>For purpose of providing the Services to you, third party service provider associates with the StreetVerse and get involved in managing, handling, storage, processing and transmitting information of StreetVerse which includes all sub-contractor, consultant and/or representatives of third party.</p>
       <br></br>
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">17. Grievance Officer</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">17. Grievance Officer</h2>
       <p>In case of any feedback or grievances in relation to the collection, storage, use, disclosure and transfer of your Personal Information under this Privacy Policy or any terms of StreetVerse’s Terms of Use and other Terms and Conditions, please contact us here <a href="/contact" class="text-blue-600 hover:text-blue-700 underline transition duration-300 ease-in-out mb-4">
         Contact Page</a></p>
       <br></br>
 
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">18. Change in this Privacy Policy</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">18. Change in this Privacy Policy</h2>
       <p>We reserve the right to change the Privacy Policy at any time by posting the amended terms on this website and we will indicate the effective date when the Privacy Policy is updated. If we make any material changes to the Privacy Policy, we will notify you by sending an e-mail to the e-mail address you most recently provided us or by posting notice of the changes on this site. We request you to periodically review the Privacy Policy from time to time for the recent changes on our privacy practices.</p>
 
-      <h2 class="text-center text-blueGray-700 underline font-bold text-xl ... ">Disclaimer</h2>
+      <h2 class="text-left text-blueGray-700 underline font-bold text-xl ... ">Disclaimer</h2>
       <p>WE MAKE NO WARRANTIES OR REPRESENTATIONS, EXPRESS OR IMPLIED. WE DO NOT HAVE ANY METHOD TO ASSESS OR VERIFY THE VERACITY OF YOUR INFORMATION PROVIDED IS CURRENT OR ACCURATE.</p>
      
     </div>
