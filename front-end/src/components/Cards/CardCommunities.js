d55ed868f84209b4cc8d0eb35958dea184de53e8
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2022-12-06 22:09:47 -0500

    Update CardCommunities.js

--- front-end/src/components/Cards/CardCommunities.js
+++ front-end/src/components/Cards/CardCommunities.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
 import CardCommunity from "./CardCommunity"
 
 const API = process.env.REACT_APP_API_URL;
