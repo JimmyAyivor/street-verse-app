4fa4a710d8ef2379ac2898ba2d1f13a17ab6f01f
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2022-12-07 20:37:55 -0500

    Update CardCommunities.js

--- front-end/src/components/Cards/CardCommunities.js
+++ front-end/src/components/Cards/CardCommunities.js
@@ -9,7 +9,7 @@ const API = process.env.REACT_APP_API_URL;
 
 export default function CardCommunities() {
     const [users, setUsers] = useState([]);
-   const title = useTitle("Our Community | StretVerse ")
+    useTitle("Your Community | StreetVerse ")
     
     useEffect(() => {
         axios
